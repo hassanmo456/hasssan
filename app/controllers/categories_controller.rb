674df7821afe399b4d5c2db4118fed282b034aab
Hassan AlMohannadi <hassan@Hassans-MacBook-Pro.local>
2020-03-21 19:21:55 +0300

    controller is done for both author and category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,47 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+    
+
+    # @categories = Categorie.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+   
+    def index
+        @categories = Category.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+    end
+    
+
+
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to category_path(@category), notice: "#{@category.name} was added to the system."
+        else
+            render action: 'new'
+        end
+    end
+
+    def update
+        if @category.update(category_params)
+            redirect_to category_path(@category), notice: "#{@category.name} was revised in the system."
+        else
+            render action: 'edit'
+        end
+    end
+    
+    def destroy
+        @category.destroy
+        redirect_to category_url
+    end
+  
+    private
+    def category_params
+        params.require(:category).permit(:name, :active)
+    end
+    
+    def set_category
+        @category = Category.find(params[:id]) 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Generated model routes
   resources :books
   # need routes for authors and categories ...
-  
+  resources :authors
+  resources :categories
   # Additional routes we've created for this project
   get 'proposed' => 'books#proposed', as: :proposed_books
   get 'contracted' => 'books#contracted', as: :contracted_books
